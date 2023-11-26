ce1fb9ba7c4322b44d2bc4ec247e87e5cb898d7a
fleonmendza <fleonmendza@gmail.com>
2023-11-26 09:42:22 -0600

    Initial Commit

---
+++ FoodMenu/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FoodMenu
+//
+//  Created by Esteban Federico León Mendoza on 26/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FoodMenu/FoodMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodMenuApp.swift
+//  FoodMenu
+//
+//  Created by Esteban Federico León Mendoza on 26/11/23.
+//
+
+import SwiftUI
+
+@main
+struct FoodMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
